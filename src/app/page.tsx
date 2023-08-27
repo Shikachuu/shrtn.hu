8b01259909f936b3e84e886e4503812e023b1419
Czékus Máté <mate@picloud.hu>
2023-08-27 10:26:33 +0200

    feat: add deployment scripts for cloudflare pages feat: add wrangler for local previews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next"
 import { Hero } from "./components/Hero"
+import { env } from "process"
+
+export const runtime = "edge"
 
 export const metadata: Metadata = {
   title: "shrtn",
